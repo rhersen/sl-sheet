4d49913764a4d96f6b9ab2f7fcb4cfb0f4a6bd7b
Rudolf Hersén <rudolf.hersen@schibsted.se>
2016-06-25 22:05:33 +0200

    Remove endpoint "departures"

--- requestListener.js
+++ requestListener.js
@@ -3,64 +3,14 @@ const atob = require('atob')
 const moment = require('moment')
 
 function requestListener(incomingRequest, outgoingResponse) {
-    let match
-
     if (/favicon.ico/.test(decodeURIComponent(incomingRequest.url)))
         favicon(outgoingResponse)
-    else if (match = /api\/departures\/(.+)/.exec(decodeURIComponent(incomingRequest.url)))
-        departures(match[1], outgoingResponse)
-    else if (/ingela/.test(decodeURIComponent(incomingRequest.url)))
+    else
         ingela(outgoingResponse)
 }
 
-function departures(location, outgoingResponse) {
-    outgoingResponse.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
-
-    const postData = departuresQuery(location)
-    const options = {
-        hostname: 'api.trafikinfo.trafikverket.se',
-        port: 80,
-        path: '/v1.1/data.json',
-        method: 'POST',
-        headers: {
-            'Content-Length': Buffer.byteLength(postData)
-        }
-    }
-
-    const outgoingRequest = http.request(options, handleResponse)
-
-    outgoingRequest.on('error', (e) => {
-        outgoingResponse.writeHead(500, {'Content-Type': 'text/plain'})
-        outgoingResponse.end(`problem with request: ${e.message}`)
-    })
-
-    outgoingRequest.write(postData)
-    outgoingRequest.end()
-
-    function handleResponse(incomingResponse) {
-        let body = ''
-        incomingResponse.setEncoding('utf8')
-        incomingResponse.on('data', chunk => body += chunk)
-        incomingResponse.on('end', done)
-
-        function done() {
-            const parsed = JSON.parse(body)
-            const announcements = parsed.RESPONSE.RESULT[0].TrainAnnouncement
-            const a = announcements.map(announcement => ` 
-                 <td>${announcement.ActivityType}
-                 <td>${announcement.LocationSignature}
-                 <td>${announcement.ToLocation[0].LocationName}
-                 <td>${announcement.AdvertisedTimeAtLocation}
-                 `)
-            outgoingResponse.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
-            outgoingResponse.end(`<table>${a.join('<tr>')}`)
-        }
-    }
-}
-
 function ingela(outgoingResponse) {
-    outgoingResponse.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
-    const postData = ingelaQuery()
+    const postData = query()
     const options = {
         hostname: 'api.trafikinfo.trafikverket.se',
         port: 80,
@@ -72,15 +22,12 @@ function ingela(outgoingResponse) {
     }
 
     const outgoingRequest = http.request(options, handleResponse)
-
-    outgoingRequest.on('error', (e) => {
-        outgoingResponse.writeHead(500, {'Content-Type': 'text/plain'})
-        outgoingResponse.end(`problem with request: ${e.message}`)
-    })
-
+    outgoingRequest.on('error', handleError)
     outgoingRequest.write(postData)
+
     outgoingRequest.end()
-    function ingelaQuery() {
+
+    function query() {
         return `<REQUEST>
      <LOGIN authenticationkey='cfdeb57c80374fcd80ca811d2bcb561a' />
      <QUERY objecttype='TrainAnnouncement' orderBy='AdvertisedTimeAtLocation'>
@@ -123,26 +70,12 @@ function ingela(outgoingResponse) {
         incomingResponse.on('end', done)
 
         function done() {
-            const parsed = JSON.parse(body)
-            const announcements = parsed.RESPONSE.RESULT[0].TrainAnnouncement
+            const announcements = JSON.parse(body).RESPONSE.RESULT[0].TrainAnnouncement
             outgoingResponse.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+            outgoingResponse.write('<table>')
+            outgoingResponse.write('<caption>Från Tullinge</caption>')
             const sub = announcements.filter(announcement => announcement.LocationSignature === 'Sub')
             const tul = announcements.filter(announcement => announcement.LocationSignature === 'Tul')
-
-            function minutes(ankomst, avgang) {
-                const ank = ankomst.AdvertisedTimeAtLocation
-                const avg = avgang.AdvertisedTimeAtLocation
-                const ankm = moment(ank)
-                const avgm = moment(avg)
-                return ankm.diff(avgm, 'minutes')
-            }
-
-            function writeRow(ankomst, avgang) {
-                const departureTime = avgang.AdvertisedTimeAtLocation.substring(11, 16)
-                const trainIdent = ankomst.AdvertisedTrainIdent
-                const minuteDiff = minutes(ankomst, avgang)
-                outgoingResponse.write(`${departureTime} ${trainIdent} ${minuteDiff} min<br>`)
-            }
 
             sub.forEach(ankomst => {
                 const avgangs = tul.filter(avgang => minutes(ankomst, avgang) > 29)
@@ -157,44 +90,36 @@ function ingela(outgoingResponse) {
             })
 
             outgoingResponse.end()
+
+            function minutes(ankomst, avgang) {
+                const ank = ankomst.AdvertisedTimeAtLocation
+                const avg = avgang.AdvertisedTimeAtLocation
+                const ankm = moment(ank)
+                const avgm = moment(avg)
+                return ankm.diff(avgm, 'minutes')
+            }
+
+            function writeRow(ankomst, avgang) {
+                const departureTime = avgang.AdvertisedTimeAtLocation.substring(11, 16)
+                const trainIdent = ankomst.AdvertisedTrainIdent
+                const minuteDiff = minutes(ankomst, avgang) - 32
+                outgoingResponse.write('<tr>')
+                outgoingResponse.write(`<td>${departureTime}`)
+                outgoingResponse.write(`<td>${trainIdent}`)
+                outgoingResponse.write(`<td>${minuteDiff} min`)
+            }
         }
+    }
+
+    function handleError(e) {
+        outgoingResponse.writeHead(500, {'Content-Type': 'text/plain'})
+        outgoingResponse.end(`problem with request: ${e.message}`)
     }
 }
 
-function favicon(outgoingResponse) {
-    outgoingResponse.writeHead(200, {'Content-Type': 'image/x-icon'})
-    outgoingResponse.end(atob('iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T'))
+function favicon(response) {
+    response.writeHead(200, {'Content-Type': 'image/x-icon'})
+    response.end(atob('iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T'))
 }
-
-function departuresQuery(locationSignature) {
-    return `<REQUEST>
-     <LOGIN authenticationkey='cfdeb57c80374fcd80ca811d2bcb561a' />
-     <QUERY objecttype='TrainAnnouncement'>
-      <FILTER>
-       <AND>
-        <IN name='ProductInformation' value='Pendeltåg' />
-        <NE name='Canceled' value='true' />
-        <EQ name='ActivityType' value='Avgang' />
-        <EQ name='LocationSignature' value='${locationSignature}' />
-        <OR>
-         <AND>
-          <GT name='AdvertisedTimeAtLocation' value='$dateadd(-00:15:00)' />
-          <LT name='AdvertisedTimeAtLocation' value='$dateadd(00:59:00)' />
-         </AND>
-         <GT name='EstimatedTimeAtLocation' value='$dateadd(-00:15:00)' />
-        </OR>
-       </AND>
-      </FILTER>
-      <INCLUDE>LocationSignature</INCLUDE>
-      <INCLUDE>AdvertisedTrainIdent</INCLUDE>
-      <INCLUDE>AdvertisedTimeAtLocation</INCLUDE>
-      <INCLUDE>EstimatedTimeAtLocation</INCLUDE>
-      <INCLUDE>TimeAtLocation</INCLUDE>
-      <INCLUDE>ToLocation</INCLUDE>
-      <INCLUDE>ActivityType</INCLUDE>
-     </QUERY>
-    </REQUEST>`
-}
-
 
 module.exports = requestListener
