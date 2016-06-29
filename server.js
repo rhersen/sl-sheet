d983e719a1cebd53c7576c79429aec677893f22a
Rudolf Hersén <rudolf.hersen@schibsted.se>
2016-06-29 07:34:37 +0200

    Move requestListener back to server.js

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-require('http').createServer(require('./requestListener')).listen(1337, '127.0.0.1')
+const atob = require('atob')
+
+const css = require('./css')
+const ingela = require('./ingela')
+const train = require('./train')
+
+function requestListener(incomingRequest, outgoingResponse) {
+    let match
+    const url = decodeURIComponent(incomingRequest.url)
+
+    if (/favicon.ico/.test(url))
+        favicon(outgoingResponse)
+    else if (match = /(\d\d\d\d)/.exec(url))
+        train(match[1], outgoingResponse)
+    else
+        ingela(outgoingResponse)
+}
+
+function favicon(response) {
+    response.writeHead(200, {'Content-Type': 'image/x-icon'})
+    response.end(atob('iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T'))
+}
+
+require('http').createServer(requestListener).listen(1337, '127.0.0.1')
