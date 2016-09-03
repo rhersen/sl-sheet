daf35b35e16ad15ca58298457c1cba8a0571b744
Rudolf Hersén <rudolf.hersen@schibsted.se>
2016-09-03 16:15:58 +0200

    Move tests

--- ingela.js
+++ ingela.js
@@ -9,10 +9,11 @@ function ingela(outgoingResponse) {
     const postData = announcementQuery(`
         <OR>
           <EQ name='LocationSignature' value='Tul' />
+          <EQ name='LocationSignature' value='Åbe' />
           <EQ name='LocationSignature' value='Sub' />
         </OR>
-        <GT name='AdvertisedTimeAtLocation' value='$dateadd(-1:00:00)' />
-        <LT name='AdvertisedTimeAtLocation' value='$dateadd(2:00:00)' />`
+        <GT name='AdvertisedTimeAtLocation' value='$dateadd(-0:28:00)' />
+        <LT name='AdvertisedTimeAtLocation' value='$dateadd(0:28:00)' />`
     )
 
     const options = {
