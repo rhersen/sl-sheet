af115d3938b1f77a6a17dcbebe25a4fd252b1f8e
Rudolf Hersén <rudolf.hersen@schibsted.se>
2016-09-21 22:55:02 +0200

    Refactor

--- trains.js
+++ trains.js
@@ -1,31 +1,25 @@
 const filter = require('lodash.filter')
+const map = require('lodash.map')
 const uniq = require('lodash.uniq')
 
-function atSameLocation(train1, train2) {
-    return train1.LocationSignature === train2.LocationSignature && train1.ActivityType === train2.ActivityType
-}
-module.exports = (announcements) => {
-    const ids = uniq(announcements.map(a => a.AdvertisedTrainIdent))
+module.exports = (announcements) =>
+    uniq(map(announcements, 'AdvertisedTrainIdent'))
+        .sort((id1, id2) => {
+            const a1 = filter(announcements, {AdvertisedTrainIdent: id1})
 
-    ids.sort((id1, id2) => {
-        const a1 = filter(announcements, {AdvertisedTrainIdent: id1})
-        const a2 = filter(announcements, {AdvertisedTrainIdent: id2})
+            for (let i = 0; i < a1.length; i++) {
+                const a2 = filter(announcements, {
+                    AdvertisedTrainIdent: id2,
+                    LocationSignature: a1[i].LocationSignature,
+                    ActivityType: a1[i].ActivityType
+                })
 
-        for (let i = 0; i < a1.length; i++) {
-            const train1 = a1[i]
+                const time1 = a1[i].AdvertisedTimeAtLocation
 
-            for (let j = 0; j < a2.length; j++) {
-                const train2 = a2[j]
-
-                if (atSameLocation(train1, train2)) {
-                    const time1 = train1.AdvertisedTimeAtLocation
-                    const time2 = train2.AdvertisedTimeAtLocation
+                for (let j = 0; j < a2.length; j++) {
+                    const time2 = a2[j].AdvertisedTimeAtLocation
                     if (time1 < time2) return -1
                     if (time1 > time2) return 1
                 }
             }
-        }
-    })
-
-    return ids
-}
+        })
